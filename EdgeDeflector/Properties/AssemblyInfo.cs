845b89ff5b746609298f6c2884b824d57487a5e7
Daniel Aleksandersen <code@daniel.priv.no>
2017-02-14 03:45:05 +0100

    Bump version number to 1.0.1.0

--- EdgeDeflector/Properties/AssemblyInfo.cs
+++ EdgeDeflector/Properties/AssemblyInfo.cs
@@ -26,11 +26,11 @@ using System.Runtime.InteropServices;
 //
 //      Major Version
 //      Minor Version
+//      Maintenance Number
 //      Build Number
-//      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
