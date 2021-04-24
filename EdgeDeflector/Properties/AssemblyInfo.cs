6b7007e6b716bf0cf3a16a9242ac1a0945630360
Daniel Aleksandersen <code@daniel.priv.no>
2021-04-24 15:21:14 +0200

    Change app description.

--- EdgeDeflector/Properties/AssemblyInfo.cs
+++ EdgeDeflector/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EdgeDeflector")]
-[assembly: AssemblyDescription("Rewrites microsoft-edge URI addresses into standard http URI addresses.")]
+[assembly: AssemblyDescription("Open MICROSOFT-EDGE:// links in your default web browser.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EdgeDeflector")]
